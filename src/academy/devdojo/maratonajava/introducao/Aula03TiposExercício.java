ed43b02ee5305a22b4d9a7295f880c12cb9f4efa
Mateus Nepomuceno <mateusnepomuceno2000@gmail.com>
2025-05-24 21:19:01 -0300

    Revisão tipos e condicionais

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposExercício.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposExercício {
+    public static void main(String[] args) {
+
+        String data = "24/05/2025";
+        String endereço = "Rua da Luanda";
+        String nome = "Mateus";
+        double salario = 1512.00;
+
+        System.out.println("Eu "+nome+", morando no endereço "+endereço+", confirmo que recebi o salário de R$ "+salario+", na data "+data);
+    }
+}

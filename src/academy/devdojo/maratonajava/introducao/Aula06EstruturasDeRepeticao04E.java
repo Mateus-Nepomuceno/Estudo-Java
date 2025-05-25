9eff68e2895fff279c83cd76e64177c3d1c53864
Mateus Nepomuceno <mateusnepomuceno2000@gmail.com>
2025-05-25 20:57:58 -0300

    refactor: revisando exercícios de repetição

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04E.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+import java.util.Scanner;
+
+public class Aula06EstruturasDeRepeticao04E {
+    public static void main(String[] args) {
+        //Dado o valor de um carro, descubra em quantas ele pode ser parcelado
+        // Condição valorParcela >= 1000
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor do carro: ");
+        double valorTotal = scanner.nextDouble();
+
+        for (int i = 1; i <= valorTotal; i++) {
+            double valorParcela = valorTotal / i;
+            if (valorParcela < 1000) {
+               break;
+            }
+            System.out.printf("O carro pode ser dividido em %dx no valor de R$ %.2f%n",i,valorParcela);
+        }
+    }
+}

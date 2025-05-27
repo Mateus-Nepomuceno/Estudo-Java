81cbca8fb42aa748e41c3b039970aed94ccd922d
Mateus Nepomuceno <mateusnepomuceno2000@gmail.com>
2025-05-27 15:31:08 -0300

    refactor: exercício de classes

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -8,16 +8,14 @@ public class CarroTest01 {
         Carro carro2 = new Carro();
 
         carro1.nome = "Gol";
+        carro1.modelo = "Sport";
         carro1.ano = 2015;
-        carro1.modelo = "V2";
 
-        carro2.nome = "Ferrari";
-        carro2.ano = 2024;
-        carro2.modelo = "V6";
+        carro2.nome = "Lamborghini";
+        carro2.modelo = "Urus";
+        carro2.ano = 2018;
 
-        carro1 = carro2;
-
-        System.out.println("Carro 01: Nome - "+carro1.nome+". Ano - "+carro1.ano+". Modelo -"+carro1.modelo);
-        System.out.println("Carro 02: Nome - "+carro2.nome+". Ano - "+carro2.ano+". Modelo -"+carro2.modelo);
+        System.out.println("Carro 1: "+carro1.nome+" - Modelo: "+carro1.modelo+" - Ano: "+carro1.ano);
+        System.out.println("Carro 2: "+carro2.nome+" - Modelo: "+carro2.modelo+" - Ano: "+carro2.ano);
     }
 }
